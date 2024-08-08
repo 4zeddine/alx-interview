6099bada31135b0d9b62dc99fa0702f687a0e900
4zeddine <azeddine.nader@gmail.com>
2024-08-08 23:14:02 +0100

    Minimum Operations commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,7 +3,9 @@
 
 
 def minOperations(n):
-    """Calculates the fewest number of operations needed to result in exactly n H characters in the file."""
+    """Calculates the fewest number of operations needed to
+    result in exactly n H characters in the file.
+    """
     if not isinstance(n, int):
         return 0
     ops_count = 0
